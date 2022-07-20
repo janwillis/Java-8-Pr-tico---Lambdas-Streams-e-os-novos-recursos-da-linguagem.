63e3adf47e3c91c87d0e0411c458ac940c0cc21e
Jan Willis <jan123willis@gmail.com>
2022-07-20 01:44:37 -0300

    feat: codigos do capitulo 7

--- src/services/Capitulo7.java
+++ src/services/Capitulo7.java
@@ -1,6 +1,10 @@
 package services;
 
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.OptionalDouble;
+
+import Interfaces.Usuarios;
+import entity.Usuario;
 
 public class Capitulo7 extends AbstractUsuarios {
 
@@ -27,9 +31,63 @@ public class Capitulo7 extends AbstractUsuarios {
 		/*
 		 * 7.4 COLLECTORS
 		 */
-		
-		usuarios.stream().filter(u-> u.getPontos()>110).collect(Collectors.toList());
+//
+//		usuarios.stream().filter(u -> u.getPontos() > 110).collect(Collectors.toList());
+//
+		/*
+		 * 7.6 LISTE APENAS OS PONTOS DE TODOS OS USUÁRIOS COM O MAP
+		 */
+//		List<Integer> pontos = usuarios.stream()
+//				.sorted(Comparator.comparing(Usuario::getNome, String.CASE_INSENSITIVE_ORDER)).map(Usuario::getPontos)
+//				.collect(Collectors.toList());
+//
+		/*
+		 * 7.7 INTSTREAM E A FAMÍLIA DE STREAMS
+		 */
+//		IntStream	stream	=	usuarios.stream()
+//				.mapToInt(Usuario::getPontos);
+//		double pontuacaoMedia = usuarios.stream()//
+//				.mapToInt(Usuario::getPontos)//
+//				.average()//
+//				.getAsDouble();
 
+		/*
+		 * 
+		 * 7.8 O OPTIONAL EM JAVA.UTIL
+		 * 
+		 * 
+		 */
+		usuarios = new ArrayList<Usuario>();
+		/* Jeito antido de impedir a divisão por zero */
+//		double soma = 0;
+//		for (Usuario u : usuarios) {
+//			soma += u.getPontos();
+//		}
+//		double pontuacaoMedia;
+//		if (usuarios.isEmpty()) {
+//			pontuacaoMedia = 0;
+//		} else {
+//			pontuacaoMedia = soma / usuarios.size();
+//		}
+
+		/* Jeito novo de impedir a divisão por zero */
+//
+//		double media = usuarios.stream()//
+//				.mapToInt(Usuario::getPontos)//
+//				.average()//
+//				.orElse(1.0);
+		
+		double	pontuacaoMedia	=	usuarios.stream()
+				.mapToInt(Usuario::getPontos)
+				.average()
+				.orElseThrow(IllegalStateException::new);
+
+		System.out.println(pontuacaoMedia);
+
+		usuarios.stream()
+		.mapToInt(Usuario::getPontos)
+		.average()
+		.ifPresent(valor	->	janela.atualiza(valor));
 	}
 
 }
